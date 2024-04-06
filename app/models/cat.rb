772e0afefcb3970edee075e4af798ac684835644
前田岳 <no1basketballplayer15@gmail.com>
2024-04-06 23:29:42 +0900

    インラインバリデーションの実装

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,7 @@
 class Cat < ApplicationRecord
+  validates :name, presence: true
+
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 #ransack4以降は以下のコードでカラムの許可をしないと検索できない
   def self.ransackable_attributes(auth_object = nil)
     ["name", "age"]
